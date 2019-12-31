7626d4877a109c943d91b398c298544bfbfa3f74
tongmeng <linzikristy@qq.com>
2019-12-31 11:02:47 +0000

    访问策略2

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -20,8 +20,7 @@ class RedirectIfAuthenticated
     {
         if (Auth::guard($guard)->check()) {
             session()->flash('info', '您已登录，无需再次操作。');
-            return redirect('/');
-//            return redirect(RouteServiceProvider::HOME);
+            return redirect(RouteServiceProvider::HOME);
         }
 
         return $next($request);
